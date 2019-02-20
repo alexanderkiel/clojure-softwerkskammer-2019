e23774aa8787800530e110b7029d6e6635725aa5
Alexander Kiel <alexanderkiel@gmx.net>
2019-02-20 17:58:05 +0100

    Add Step 0

---
+++ src/todo_backend/step_0.clj
@@ -0,0 +1,29 @@
+(ns todo-backend.step-0)
+
+;; Simple Values
+(comment
+  ;; Integers
+  1
+
+  ;; Doubles
+  3.14
+
+  ;; Strings
+  "Hello World"
+
+  ;; Booleans
+  true
+  false
+
+  ;; nil
+  nil
+
+  )
+
+;; Function Application
+(comment
+  (inc 1)
+
+  (str "a" "b")
+
+  )
